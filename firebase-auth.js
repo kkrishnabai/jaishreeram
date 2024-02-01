0bf37762f619b99f743885d3eb90b2c05bddfcba
kkrishnabai <84125692+kkrishnabai@users.noreply.github.com>
2024-02-01 13:17:18 +0530

    Add files via upload

---
+++ firebase-auth.js
@@ -0,0 +1,40 @@
+// Sign Up Function
+function signUp() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("User signed up:", user.email);
+    })
+    .catch((error) => {
+      console.error("Sign up error:", error.message);
+    });
+}
+
+// Login Function
+function login() {
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("User logged in:", user.email);
+    })
+    .catch((error) => {
+      console.error("Login error:", error.message);
+    });
+}
+
+// Sign Out Function
+function signOut() {
+  firebase.auth().signOut()
+    .then(() => {
+      console.log("User signed out");
+    })
+    .catch((error) => {
+      console.error("Sign out error:", error.message);
+    });
+}
---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Replace with your Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyDsj4I_h7jTgsfIvakEbY3ojgMnDz4NYQY",
+    authDomain: "krishna1-48406.firebaseapp.com",
+    projectId: "krishna1-48406",
+    storageBucket: "krishna1-48406.appspot.com",
+    messagingSenderId: "268784196182",
+    appId: "1:268784196182:web:f421e52b9ea8945b35f9f7",
+    measurementId: "G-1YLDCEJHGS"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
