6a1a7e8b904dc77498de69cfbd7f0b8bd78e0c60
kkrishnabai <84125692+kkrishnabai@users.noreply.github.com>
2024-02-01 13:44:10 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -12,6 +12,6 @@ const firebaseConfig = {
   };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
