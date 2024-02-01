2ffd69fcfa880257b112f6fb3aae8196426865a5
kkrishnabai <84125692+kkrishnabai@users.noreply.github.com>
2024-02-01 14:07:42 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,15 @@
 // Replace with your Firebase Config
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDsj4I_h7jTgsfIvakEbY3ojgMnDz4NYQY",
-    authDomain: "krishna1-48406.firebaseapp.com",
-    projectId: "krishna1-48406",
-    storageBucket: "krishna1-48406.appspot.com",
-    messagingSenderId: "268784196182",
-    appId: "1:268784196182:web:f421e52b9ea8945b35f9f7",
-    measurementId: "G-1YLDCEJHGS"
-  };
+  apiKey: "AIzaSyDsj4I_h7jTgsfIvakEbY3ojgMnDz4NYQY",
+  authDomain: "krishna1-48406.firebaseapp.com",
+  projectId: "krishna1-48406",
+  storageBucket: "krishna1-48406.appspot.com",
+  messagingSenderId: "268784196182",
+  appId: "1:268784196182:web:f421e52b9ea8945b35f9f7",
+  measurementId: "G-1YLDCEJHGS"
+};
 
 // Initialize Firebase
 const auth = firebase.auth();
